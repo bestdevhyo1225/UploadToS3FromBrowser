191d62348f06576c1b311cb8d6d5a804d71a9b66
janghyoseog <bestdevhyo1225@gmail.com>
2019-10-01 14:58:44 +0900

    feat: Add 'async-exception', 'error-handler' middleware

---
+++ src/controllers/indexController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export const index = async (req: Request, res: Response): Promise<void> => {
+    res.render('index');
+}
+
+export default {
+    index,
+}
--- src/controllers/uploadController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const upload = async (req: Request, res: Response): Promise<void> => {
-    res.render('upload');
-}
-
-export default {
-    upload,
-}
